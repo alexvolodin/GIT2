0ba011197096c83accca36057b62efe275813b22
alexvolodin <166716843+alexvolodin@users.noreply.github.com>
2024-07-23 14:45:58 +0300

    commited

---
+++ GIT.java
@@ -0,0 +1,12 @@
+package git;
+
+public class GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow world1");
+
+        System.out.println("Update1");
+
+        System.out.println("AGAIN UPDATE");
+    }
+}
